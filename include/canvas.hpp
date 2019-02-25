3dedcbd900a97bdbd26f7733345b2920d05f0d86
Josivan Medeiros <jimmy@pop-os.localdomain>
2019-02-25 16:44:32 -0300

    Bresenhams line

--- include/canvas.hpp
+++ include/canvas.hpp
@@ -1,10 +1,15 @@
+#ifndef __CANVAS_HPP__
+#define __CANVAS_HPP__
+
 #include <string>
 #include <fstream>
 #include <iostream>
+
+#define CHANNELS 3
 #define PRINTPXL(x,y,c) {\
-  _pixels[3*(x+y*_w)] = c[0];\
-  _pixels[3*(x+y*_w)+1] = c[1];\
-  _pixels[3*(x+y*_w)+2] = c[2];\
+  _pixels[CHANNELS*(x+y*_w)] = c[0];\
+  _pixels[CHANNELS*(x+y*_w)+1] = c[1];\
+  _pixels[CHANNELS*(x+y*_w)+2] = c[2];\
 }
 
 class Canvas
@@ -13,12 +18,16 @@ class Canvas
    int _w, _h;
    unsigned char * _pixels;
    void scanline();
+   void bresenham(int x1, int y1, int x2, int y2);
+   void midptellipse(int rx, int ry, int xc, int yc);
  public:
    Canvas(int width, int height) : _w{width}, _h{height},
-     _pixels{new unsigned char[width*height*3]} {/*Empty*/}
+     _pixels{new unsigned char[width*height*CHANNELS]} {/*Empty*/}
    void draw();
    void imwrite(std::string filename);
    void rect(int width, int height, int cx=0, int cy=0);
    void line(int x1, int y1, int x2, int y2);
    void poly(int * coords);
 };
+
+#endif
--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,5 +1,10 @@
 #include "canvas.hpp"
 
+unsigned char WHITE[3] = {255, 255, 255};
+unsigned char RED[3]   = {255,   0,   0};
+unsigned char GREEN[3] = {  0, 255,   0};
+unsigned char BLUE[3]  = {  0,   0, 255};
+
 void Canvas::draw()
 {
   // @TODO Call draw function in a vector of drawable entities?
@@ -7,11 +12,12 @@ void Canvas::draw()
   {
     _pixels[i] = 150;
   }
-  int white[3] = {255,0,0};
   for ( int i = 0; i < 50; ++i )
   {
-    PRINTPXL(10+i,10,white);
+    PRINTPXL(10+i,10,WHITE);
   }
+  bresenham(10,10, 300, 300);
+  midptellipse(100, 100, 200, 200);
 }
 
 void Canvas::imwrite(std::string filename)
@@ -27,4 +33,80 @@ void Canvas::imwrite(std::string filename)
     fs << (int) _pixels[i] << "\n";
   }
   fs.close();
+}
+/**
+ * Bresenham Algorithm to draw lines
+ */
+void Canvas::bresenham(int x1, int y1, int x2, int y2) {
+  int m_new = 2 * (y2 - y1);
+  int slope_error_new = m_new - (x2 - x1);
+  int red[3] = {255,0,0};
+  for (int x = x1, y = y1; x <= x2; ++x) {
+    PRINTPXL(x,y,RED);
+    slope_error_new += m_new;
+    if (slope_error_new >= 0) {
+      ++y;
+      slope_error_new -= 2 * (x2 - x1);
+    }
+  }
+}
+/**
+ * Mid point algorithm to draw ellipse
+ */
+void Canvas::midptellipse(int rx, int ry, int xc, int yc) {
+    float dx, dy, d1, d2, x, y;
+    x = 0;
+    y = ry;
+    // Initial decision parameter of region 1
+    d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx);
+    dx = 2 * ry * ry * x;
+    dy = 2 * rx * rx * y;
+    // For region 1
+    while (dx < dy) {
+        // Print points based on 4-way symmetry
+        PRINTPXL((int) x + xc,(int) y + yc, GREEN );
+        PRINTPXL((int) -x + xc,(int) y + yc, GREEN);
+        PRINTPXL((int) x + xc, (int)-y + yc, GREEN);
+        PRINTPXL((int) -x + xc, (int)-y + yc, GREEN);
+
+        // Checking and updating value of
+        // decision parameter based on algorithm
+        if (d1 < 0) {
+            x++;
+            dx = dx + (2 * ry * ry);
+            d1 = d1 + dx + (ry * ry);
+        }
+        else {
+            x++;
+            y--;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d1 = d1 + dx - dy + (ry * ry);
+        }
+    }
+    // Decision parameter of region 2
+    d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) +
+         ((rx * rx) * ((y - 1) * (y - 1))) -
+          (rx * rx * ry * ry);
+    // Plotting points of region 2
+    while (y >= 0) {
+        PRINTPXL((int) x + xc,(int) y + yc, GREEN );
+        PRINTPXL((int) -x + xc,(int) y + yc, GREEN);
+        PRINTPXL((int) x + xc,(int) -y + yc, GREEN);
+        PRINTPXL((int) -x + xc,(int) -y + yc, GREEN);
+        // Checking and updating parameter
+        // value based on algorithm
+        if (d2 > 0) {
+            y--;
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + (rx * rx) - dy;
+        }
+        else {
+            y--;
+            x++;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + dx - dy + (rx * rx);
+        }
+    }
 }
